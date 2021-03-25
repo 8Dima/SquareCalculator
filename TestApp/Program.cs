b5f1b5b62e28747435612fa35280c0ce5fc53cb6
Dmitriy <smphag@gmail.com>
2021-03-25 21:39:14 +0300

    Adding files

---
+++ TestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using squareCalc;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите радиус круга, либо длины 3 сторон треугольника через пробел");
+            string input = Console.ReadLine();
+            string[] inputArr = input.Split(' ');
+            if (inputArr.Length == 1)
+            {
+                squareCalc.Calc calc = new Calc(double.Parse(inputArr[0]));
+                calc.Circle();
+            }
+            else if (inputArr.Length == 3)
+            {
+                squareCalc.Calc calc = new Calc(double.Parse(inputArr[0]), double.Parse(inputArr[1]), double.Parse(inputArr[2]));
+                calc.Triangle();
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+        }
+    }
+}
---
+++ squareCalc/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace squareCalc
+{
+    public class Calc
+    {
+        public double radius;
+        public double side1;
+        public double side2;
+        public double side3;
+        
+        public Calc(double r)
+        {
+            radius = r;
+        }
+
+        public Calc(double s1, double s2, double s3)
+        {
+            side1 = s1;
+            side2 = s2;
+            side3 = s3;
+        }
+
+        public void Triangle()
+        {
+            double p = (side1 + side2 + side3) / 2.0;
+            double res = Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3));
+            Console.WriteLine(p);
+            Console.WriteLine(res);
+        }
+
+        public void Circle()
+        {
+            double res = Math.Pow(radius, 2) * Math.PI;
+            Console.WriteLine(res);
+        }
+    }
+}
